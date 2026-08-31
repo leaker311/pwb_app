ccc1abc0978b036148dcb87891a1f45f69833806
leaker311 <co_vd_b@yahoo.com>
2026-08-31 12:49:36 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const LINES = [
+  "----+----1----+----2----+",
+  "",
+  "SOULS- - - - ",
+  "",
+  "COL A     COL B     COL C",
+  "111       222       333",
+  "",
+  "IF COLUMNS LINE UP, GOOD",
+];
+
+document.getElementById("screen").textContent = LINES.join("\n");
